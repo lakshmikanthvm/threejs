5e9880af52a72acd4ec5aaa0841d8f5143a20882
lk@mizzendigital.com <lakshmikanthvasudev@gmail.com>
2025-06-28 19:08:34 +0530

    initial commit

---
+++ react-3d/src/App.jsx
@@ -0,0 +1,35 @@
+import { useRef } from 'react';
+import { Canvas, useFrame } from '@react-three/fiber';
+import { OrbitControls, Sparkles } from '@react-three/drei';
+
+const RotatingCube = () => {
+  const meshRef = useRef();
+
+  useFrame(() => {
+    if (meshRef.current) {
+      meshRef.current.rotation.x += 0.01;
+      meshRef.current.rotation.y += 0.01;
+    }
+  });
+
+  return (
+    <mesh ref={meshRef}>
+      <cylinderGeometry args={[1, 1, 1]} />
+      <meshStandardMaterial color='#468585' emissive='#468585' />
+      <Sparkles add={true} size={6} scale={[10, 10, 10]} position={[0, 0, 0]} speed={1} count={100} color="#9CDBA6" />
+    </mesh>
+  );
+}
+
+const App = () => {
+  return (
+    <Canvas style={{ height: '100vh', width: '100vw', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+      <OrbitControls enableZoom enablePan enableRotate />
+      <directionalLight position={[1, 1, 1]} intensity={1} color={0x9CDBA6} />
+      <color attach="background" args={['#F0F0F0']} />
+      <RotatingCube />
+    </Canvas>
+  );
+}
+
+export default App;
